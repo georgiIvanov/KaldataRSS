0763e8734857f75f4c06393f9f86662ea5fc9fa4
Георги <georgi.ivanovtn@gmail.com>
2014-02-22 10:41:22 +0200

    initial commit

---
+++ KaldataRSS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KaldataRSS
+//
+//  Created by Georgi Ivanov on 2/22/14.
+//  Copyright (c) 2014 Georgi Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ KaldataRSSTests/KaldataRSSTests.m
@@ -0,0 +1,34 @@
+//
+//  KaldataRSSTests.m
+//  KaldataRSSTests
+//
+//  Created by Georgi Ivanov on 2/22/14.
+//  Copyright (c) 2014 Georgi Ivanov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KaldataRSSTests : XCTestCase
+
+@end
+
+@implementation KaldataRSSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
